47749a9f51abae64ed9465d031a8c89f0ac7f5cc
markusphil <m.philipp1991@web.de>
2020-08-29 19:17:37 +0200

    load data via graphQL

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,26 +1,43 @@
 import PropTypes from "prop-types"
 import React from "react"
 import ContactBox from "./cv/contactBox"
+import { graphql, useStaticQuery } from "gatsby"
 
-import contactData from "../jsonDataSource/contact.json"
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContactJson {
+        edges {
+          node {
+            id
+            name
+            icon
+            url
+          }
+        }
+      }
+    }
+  `)
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-      display: `flex`,
-      justifyContent: `space-between`,
-    }}
-  >
-    <div>
-      <h1 style={{ margin: 0 }}>{siteTitle}</h1>
-      <p class="subtitle">Student Web Developer</p>
-    </div>
-    <ContactBox entries={contactData} />
-  </header>
-)
+  console.log(data)
 
+  return (
+    <header
+      style={{
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
+        display: `flex`,
+        justifyContent: `space-between`,
+      }}
+    >
+      <div>
+        <h1 style={{ margin: 0 }}>{siteTitle}</h1>
+        <p className="subtitle">Student Web Developer</p>
+      </div>
+      <ContactBox entries={data.allContactJson.edges.map(edge => edge.node)} />
+    </header>
+  )
+}
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -7,24 +7,62 @@ import TimeLine from "../components/cv/timeline/timeLine"
 import SkillList from "../components/cv/skills/SkillList"
 import TabList from "../components/general/tabList"
 
-import education from "../jsonDataSource/education.json"
-import practical from "../jsonDataSource/practical.json"
-import skills from "../jsonDataSource/skills.json"
-
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <div class="col-lg-8">
       <TabList>
-        <TimeLine title={"Education"} entries={education} />
-        <TimeLine title={"Practical Experiances"} entries={practical} />
+        <TimeLine
+          title={"Education"}
+          entries={data.allEducationJson.edges.map(e => e.node)}
+        />
+        <TimeLine
+          title={"Practical Experiances"}
+          entries={data.allPracticalJson.edges.map(e => e.node)}
+        />
         <div title={"TEST"}>TEST</div>
       </TabList>
     </div>
     <div class="col-lg-4">
-      <SkillList skillCats={skills} />
+      <SkillList skillCats={data.allSkillsJson.edges.map(e => e.node)} />
     </div>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allEducationJson {
+      edges {
+        node {
+          desc
+          start
+          title
+          end
+        }
+      }
+    }
+    allPracticalJson {
+      edges {
+        node {
+          desc
+          start
+          title
+          end
+        }
+      }
+    }
+    allSkillsJson {
+      edges {
+        node {
+          category
+          skills {
+            name
+            rating
+          }
+        }
+      }
+    }
+  }
+`
